07cbda50cbe0e79ae1841a68249be8ff5ddb9078
carlfre <carlfrederiknk@gmail.com>
2021-09-22 16:29:38 +0200

    Created sketch for create_tridiagonal(vectors a, d, e)

--- src/create_tridiagonal.cpp
+++ src/create_tridiagonal.cpp
@@ -4,17 +4,29 @@
 // - lower diagonal: vector a, lenght n-1
 // - main diagonal:  vector d, lenght n
 // - upper diagonal: vector e, lenght n-1
-arma::mat create_tridiagonal(const arma::vec& a, const arma::vec& d, const arma::vec& e)
+arma::mat create_tridiagonal(const arma::vec& a, const arma::vec& d,const arma::vec& e)
 {
-  // Start from identity matrix
-  // arma::mat A = arma::mat(n, n, arma::fill::eye);
+    // Start from identity matrix
+    int n = d.n_elem;
+    arma::mat A = arma::mat(n, n, arma::fill::eye);
+    
+    // Fill first row (row index 0)
+    A(0, 0) = d(0);
+    A(0, 1) = e(0);
 
-  // Fill first row (row index 0)
+    // Loop that fills rows 2 to n-1 (row indices 1 to n-2)
+    for (i = 1;i < n-1; i++)
+    {
+        A(i, i-1) = a(i-1);
+        A(i, i) = d(i);
+        A(i, i+1) = e(i);
+    }
 
-  // Loop that fills rows 2 to n-1 (row indices 1 to n-2)
-  
-  // Fill last row (row index n-1)
-    arma::mat A;
+    // Fill last row (row index n-1)
+    A(n-1, n-2) = a(n-2);
+    A(n-1, n-1) = d(n-1);
+
+    arma::mat A; //hvorfor?
     return A;
 }
 
@@ -23,12 +35,12 @@ arma::mat create_tridiagonal(const arma::vec& a, const arma::vec& d, const arma:
 // from scalar input a, d and e
 arma::mat create_tridiagonal(int n, double a, double d, double e)
 {
-  arma::vec a_vec = arma::vec(n-1, arma::fill::ones) * a;
-  arma::vec d_vec = arma::vec(n, arma::fill::ones) * d;
-  arma::vec e_vec = arma::vec(n-1, arma::fill::ones) * e;
+    arma::vec a_vec = arma::vec(n-1, arma::fill::ones) * a;
+    arma::vec d_vec = arma::vec(n, arma::fill::ones) * d;
+    arma::vec e_vec = arma::vec(n-1, arma::fill::ones) * e;
 
-  // Call the vector version of this function and return the result
-  return create_tridiagonal(a_vec, d_vec, e_vec);
+    // Call the vector version of this function and return the result
+    return create_tridiagonal(a_vec, d_vec, e_vec);
 }
 
 
@@ -36,6 +48,6 @@ arma::mat create_tridiagonal(int n, double a, double d, double e)
 // from scalar input a and d.
 arma::mat create_symmetric_tridiagonal(int n, double a, double d)
 {
-  // Call create_tridiagonal and return the result
-  return create_tridiagonal(n, a, d, a);
+    // Call create_tridiagonal and return the result
+    return create_tridiagonal(n, a, d, a);
 }
