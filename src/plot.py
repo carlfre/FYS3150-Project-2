8e7b5bfcdfa198463d08c5a822df0bb90f27a719
didriksi <didriksten@gmail.com>
2021-09-27 14:17:27 +0200

    Add plot for problem 6, and cla for choosing what to plot

--- src/plot.py
+++ src/plot.py
@@ -4,21 +4,28 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_number_of_transformations(filename):
+    """Plots the number of transformations vs N"""
+    data = np.loadtxt(f"data/{filename}", dtype=int, delimiter=",")
+    plt.scatter(*data.T)
+    plt.title("Similarity transformations needed to solve system")
+    plt.xlabel("N")
+    plt.ylabel("Transformations")
+    plt.savefig("plots/similarity.pdf")
+
 def read_3_eigenvalues_and_eigenvectors_from_file(filename):
     """Reads values of 3 eigenvalues and eigenvectors from csv-file and returns them"""
-    data = np.loadtxt(filename, dtype="float", delimiter=",")
+    data = np.loadtxt(f"data/{filename}", dtype="float", delimiter=",")
     eigenvalues = data[0, :]
     v1 = data[1:, 0]
     v2 = data[1:, 1]
     v3 = data[1:, 2]
     return eigenvalues, v1, v2, v3
 
-def plot_buckling_beam_solution(file_name):
+def plot_buckling_beam_solution(filename):
     """Plots approximated solution to buckling beam problem for parameter lambda (eigenvalue)."""
     #Retrieve data from "data"-folder
-    os.chdir("..")
-    os.chdir("data")
-    eigenvalues, v1, v2, v3 = read_3_eigenvalues_and_eigenvectors_from_file(file_name)
+    eigenvalues, v1, v2, v3 = read_3_eigenvalues_and_eigenvectors_from_file(filename)
     N = len(v1)
     n = N + 1
     fig, axs = plt.subplots(3, sharex=True, sharey=True)
@@ -33,13 +40,17 @@ def plot_buckling_beam_solution(file_name):
         axs[i].plot(x, solution, label = f"$\lambda_{i+1}$={eigenvalues[i]:.2f}")
         axs[i].legend()
     #save plots in "plots"-folder
-    os.chdir("..")
-    os.chdir("plots")
-    plt.savefig(f"solution_for_N_equals{N}.pdf")
+    plt.savefig(f"plots/solution_for_N_equals{N}.pdf")
+
+def main():
+    if sys.argv[1] == "problem6":
+        plot_number_of_transformations("number_of_iterations.csv")
+    elif sys.argv[1] == "problem7":
+        file_names = ["3_eigvecs_n_equals_10.csv", "3_eigvecs_n_equals_100.csv"]
+        for file in file_names:
+            plot_buckling_beam_solution(file)
 
 if __name__ == "__main__":
-    file_names = ["3_eigvecs_n_equals_10.csv", "3_eigvecs_n_equals_100.csv"]
-    for file in file_names:
-        plot_buckling_beam_solution(file)
-
+    import sys
+    main()
 
